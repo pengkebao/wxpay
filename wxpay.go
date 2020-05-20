cc6b0e2dc244af9bf7a84ed5c1424dffa6641bb6
pengkebao <84665381@qq.com>
2020-05-20 09:40:46 +0800

    add appApiPay
    add appApiPay

--- wxpay.go
+++ wxpay.go
@@ -301,6 +301,26 @@ func JsApiPay(config WxPayConfig, prepayId string) (string, error) {
 	return string(body), nil
 }
 
+// 应用ID	appid	String(32)	是	wx8888888888888888	微信开放平台审核通过的应用APPID
+// 商户号	partnerid	String(32)	是	1900000109	微信支付分配的商户号
+// 预支付交易会话ID	prepayid	String(32)	是	WX1217752501201407033233368018	微信返回的支付交易会话ID
+// 扩展字段	package	String(128)	是	Sign=WXPay	暂填写固定值Sign=WXPay
+// 随机字符串	noncestr	String(32)	是	5K8264ILTKCH16CQ2502SI8ZNMTM67VS	随机字符串，不长于32位。推荐随机数生成算法
+// 时间戳	timestamp	String(10)	是	1412000000	时间戳，请见接口规则-参数规定
+// 签名	sign	String(32)	是	C380BEC2BFD727A4B6845133519F3AD6	签名，详见签名生成算法注意：签名方式一定要与统一下单接口使用的一
+func AppApiPay(config WxPayConfig, prepayId string) map[string]string {
+	timeStamp := fmt.Sprintf("%d", time.Now().Unix())
+	appApiObj := make(map[string]string)
+	appApiObj["appid"] = config.AppId
+	appApiObj["partnerid"] = config.MchId
+	appApiObj["prepayid"] = prepayId
+	appApiObj["package"] = "Sign=WXPay"
+	appApiObj["nonceStr"] = getNonceStr(32) //随机字符串
+	appApiObj["timeStamp"] = timeStamp
+	appApiObj["sign"] = makeSign(appApiObj, config.AppKey)
+	return appApiObj
+}
+
 //刷卡支付订单提交
 func MicroPay(config WxPayConfig, params map[string]string) (resp MicroPayResponse, err error) {
 	if params["out_trade_no"] == "" {
